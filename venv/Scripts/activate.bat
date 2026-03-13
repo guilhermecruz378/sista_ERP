c0799040f7da88b5ef2476f5a594ca02fd71dbcb
Guilherme Cruz <guilhermecruz378@gmail.com>
2026-03-13 18:39:16 -0300

    exclusão

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\guilh\OneDrive\Área de Trabalho\davi_igreja\venv
+set VIRTUAL_ENV=C:\Users\guilh\OneDrive\Área de Trabalho\subir_para_o_git\sista_ERP\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
