5e22ad0e07235958a335e0262f1596567e0a0164
Guilherme Cruz <guilhermecruz378@gmail.com>
2026-03-15 06:44:57 -0300

    atualizações da madrugada, para o sistema voltar a funcionar

--- sistema_web/core/forms.py
+++ sistema_web/core/forms.py
@@ -12,10 +12,9 @@ class ProdutoForm(forms.ModelForm):
         }
 
     # TRUQUE DE MESTRE: Aplica a classe CSS 'form-control' em TODOS os campos automaticamente
-def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ProdutoForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             # Preserva as classes que já existem (como o type date) e adiciona o form-control
             classes_atuais = field.widget.attrs.get('class', '')
             field.widget.attrs['class'] = f'{classes_atuais} form-control'.strip()
-        
